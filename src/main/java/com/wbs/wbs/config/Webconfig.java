aa0c550b2f309c34bb1ed618abdaeeaf79f74c9f
parkjunjae <wnswo1109@naver.com>
2025-05-08 15:17:01 +0900

    웹 설정

---
+++ src/main/java/com/wbs/wbs/config/Webconfig.java
@@ -0,0 +1,19 @@
+package com.wbs.wbs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class Webconfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("*")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("*");
+
+    }
+    
+}
