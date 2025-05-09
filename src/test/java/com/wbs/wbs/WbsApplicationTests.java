665e70b8515c988f92321895e9dc040b152ba3b3
parkjunjae <wnswo1109@naver.com>
2025-05-09 14:04:36 +0900

    확인

--- src/test/java/com/wbs/wbs/WbsApplicationTests.java
+++ src/test/java/com/wbs/wbs/WbsApplicationTests.java
@@ -1,6 +1,5 @@
 package com.wbs.wbs;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,11 +17,6 @@ class WbsApplicationTests {
 	}
 
 
-	@Test
-	void getTest(){
-		
-		assertEquals(0, 0);
 
-	}
 
 }
